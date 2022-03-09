f3712b84c62871da7041ecb843a979d4576d02bb
Bozana Bokan <bozana.bokan@posteo.net>
2022-03-09 14:43:27 +0100

    metrics tables and migration

--- classes/core/Application.inc.php
+++ classes/core/Application.inc.php
@@ -123,6 +123,9 @@ class Application extends PKPApplication
             'MetricsDAO' => 'APP\statistics\MetricsDAO',
             'OAIDAO' => 'APP\oai\ops\OAIDAO',
             'SectionDAO' => 'APP\server\SectionDAO',
+            'UsageStatsTotalTemporaryRecordDAO' => 'APP\statistics\UsageStatsTotalTemporaryRecordDAO',
+            'UsageStatsUniqueItemInvestigationsTemporaryRecordDAO' => 'APP\statistics\UsageStatsUniqueItemInvestigationsTemporaryRecordDAO',
+            'UsageStatsUniqueItemRequestsTemporaryRecordDAO' => 'APP\statistics\UsageStatsUniqueItemRequestsTemporaryRecordDAO',
         ]);
     }
 
