5401949f84df64bffa44aa0df09e6751e0356128
Bozana Bokan <bozana.bokan@posteo.net>
2022-06-13 16:02:28 +0200

    jobs

--- Jobs/Statistics/CompileMonthlyMetrics.php
+++ Jobs/Statistics/CompileMonthlyMetrics.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * @file Jobs/Statistics/LoadMonthlyMetricsDataJob.php
+ * @file Jobs/Statistics/CompileMonthlyMetrics.php
  *
  * Copyright (c) 2014-2022 Simon Fraser University
  * Copyright (c) 2000-2022 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
- * @class LoadMonthlyMetricsDataJob
+ * @class CompileMonthlyMetrics
  * @ingroup jobs
  *
  * @brief Class to monthly aggregate the usage metrics data as a Job
@@ -15,24 +15,35 @@
 
 namespace APP\Jobs\Statistics;
 
-use APP\core\Application;
 use APP\core\Services;
+use PKP\site\Site;
 use PKP\Support\Jobs\BaseJob;
 
-class LoadMonthlyMetricsDataJob extends BaseJob
+class CompileMonthlyMetrics extends BaseJob
 {
     /**
-     * The month the usage metrics should be aggregated by
+     * The number of times the job may be attempted.
+     */
+    public $tries = 1;
+
+    /**
+     * The month the usage metrics should be aggregated by, in format YYYYMM.
      */
     protected string $month;
 
+
+    protected Site $site;
+
     /**
      * Create a new job instance.
+     *
+     * @param string $month In format YYYYMM
      */
-    public function __construct(string $month)
+    public function __construct(string $month, Site $site)
     {
         parent::__construct();
         $this->month = $month;
+        $this->site = $site;
     }
 
     /**
@@ -40,22 +51,17 @@ class LoadMonthlyMetricsDataJob extends BaseJob
      */
     public function handle(): void
     {
-        $application = Application::get();
-        $request = $application->getRequest();
-        $site = $request->getSite();
         $currentMonth = date('Ym'); // shall we consider only current month or maybe rather previous month?
 
-        // geo
         $geoService = Services::get('geoStats');
-        $geoService->aggregateMetrics($this->month);
-        if (!$site->getData('usageStatsKeepDaily') && $this->month != $currentMonth) {
+        $geoService->addMonthlyMetrics($this->month);
+        if (!$this->site->getData('keepDailyUsageStats') && $this->month != $currentMonth) {
             $geoService->deleteDailyMetrics($this->month);
         }
 
-        // COUNTER submissions and insitutions
         $counterService = Services::get('sushiStats');
-        $counterService->aggregateMetrics($this->month);
-        if (!$site->getData('usageStatsKeepDaily') && $this->month != $currentMonth) {
+        $counterService->addMonthlyMetrics($this->month);
+        if (!$this->site->getData('keepDailyUsageStats') && $this->month != $currentMonth) {
             $counterService->deleteDailyMetrics($this->month);
         }
     }
